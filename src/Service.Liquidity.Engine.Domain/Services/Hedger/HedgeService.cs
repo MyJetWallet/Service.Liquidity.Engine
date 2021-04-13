ce05fefd03093b865abadc4c7cc91660412a44f1
Alexey Novichikhin <novichikhin.au@gmail.com>
2021-04-13 16:46:46 +0300

    update

--- src/Service.Liquidity.Engine.Domain/Services/Hedger/HedgeService.cs
+++ src/Service.Liquidity.Engine.Domain/Services/Hedger/HedgeService.cs
@@ -58,12 +58,14 @@ namespace Service.Liquidity.Engine.Domain.Services.Hedger
         private async Task HedgePositionAsync(PositionPortfolio positionPortfolio)
         {
             using var activity = MyTelemetry.StartActivity("Hedge portfolio position");
-            
+
             activity?.AddTag("positionId", positionPortfolio.Id)
-                     .AddTag("symbol", positionPortfolio.Symbol);
+                .AddTag("symbol", positionPortfolio.Symbol);
 
             var settings = _settingsManager.GetGlobalHedgeSettings();
-            var instrumentSettings = _instrumentSettingsManager.GetHedgeInstrumentSettings(positionPortfolio.Symbol, positionPortfolio.WalletId);
+            var instrumentSettings =
+                _instrumentSettingsManager.GetHedgeInstrumentSettings(positionPortfolio.Symbol,
+                    positionPortfolio.WalletId);
 
             if (settings == null || instrumentSettings == null)
             {
@@ -71,52 +73,60 @@ namespace Service.Liquidity.Engine.Domain.Services.Hedger
                 return;
             }
 
-            if (settings.Mode != EngineMode.Active)
-            {
-                activity?.AddTag("hedge-mode", settings.Mode.ToString());
-                activity?.AddTag("hedge-message", "global disabled");
-                return;
-            }
-
-            if (instrumentSettings.Mode != EngineMode.Active)
-            {
-                activity?.AddTag("hedge-mode", instrumentSettings.Mode.ToString());
-                activity?.AddTag("hedge-message", "instrument disabled");
-                return;
-            }
-
-            var wallet = _lpWalletManager.GetWalletById(instrumentSettings.WalletId);
-            if (wallet == null)
-            {
-                _logger.LogError("Cannot hedge position {positionId}, because wallet '{marketName}' not found", positionPortfolio.Id, instrumentSettings.WalletId);
-                activity?.AddTag("hedge-message", "lp wallet not found");
-                activity?.SetStatus(Status.Error);
-                return;
-            }
-
-            activity?.AddTag("walletName", wallet.Name);
-            
-            var market = _externalMarketManager.GetExternalMarketByName(instrumentSettings.ExternalMarket);
-
-            if (market == null)
-            {
-                activity?.AddTag("hedge-message", "external market do not found");
-                _logger.LogError("Cannot hedge position {positionId}, because external market '{marketName}' not found", positionPortfolio.Id, instrumentSettings.ExternalMarket);
-                activity?.SetStatus(Status.Error);
-                return;
-            }
-
-            activity?.AddTag("external-market", instrumentSettings.ExternalMarket);
-
-            var hedgeSide = positionPortfolio.Side == OrderSide.Buy ? OrderSide.Sell : OrderSide.Buy;
-            var hedgeVolume = (double)(-positionPortfolio.BaseVolume);
-            var hedgeReferenceId = GenerateReferenceId(positionPortfolio);
-
-            _logger.LogInformation("Try to hedge position: {jsonText}", JsonConvert.SerializeObject(positionPortfolio));
-
             try
             {
-                new { instrumentSettings.ExternalMarket, instrumentSettings.ExternalSymbol, hedgeSide, hedgeVolume, hedgeReferenceId }.AddToActivityAsJsonTag("external-trade-request");
+                if (settings.Mode != EngineMode.Active)
+                {
+                    activity?.AddTag("hedge-mode", settings.Mode.ToString());
+                    activity?.AddTag("hedge-message", "global disabled");
+                    return;
+                }
+
+                if (instrumentSettings.Mode != EngineMode.Active)
+                {
+                    activity?.AddTag("hedge-mode", instrumentSettings.Mode.ToString());
+                    activity?.AddTag("hedge-message", "instrument disabled");
+                    return;
+                }
+
+                var wallet = _lpWalletManager.GetWalletById(instrumentSettings.WalletId);
+                if (wallet == null)
+                {
+                    _logger.LogError("Cannot hedge position {positionId}, because wallet '{marketName}' not found",
+                        positionPortfolio.Id, instrumentSettings.WalletId);
+                    activity?.AddTag("hedge-message", "lp wallet not found");
+                    activity?.SetStatus(Status.Error);
+                    return;
+                }
+
+                activity?.AddTag("walletName", wallet.Name);
+
+                var market = _externalMarketManager.GetExternalMarketByName(instrumentSettings.ExternalMarket);
+
+                if (market == null)
+                {
+                    activity?.AddTag("hedge-message", "external market do not found");
+                    _logger.LogError(
+                        "Cannot hedge position {positionId}, because external market '{marketName}' not found",
+                        positionPortfolio.Id, instrumentSettings.ExternalMarket);
+                    activity?.SetStatus(Status.Error);
+                    return;
+                }
+
+                activity?.AddTag("external-market", instrumentSettings.ExternalMarket);
+
+                var hedgeSide = positionPortfolio.Side == OrderSide.Buy ? OrderSide.Sell : OrderSide.Buy;
+                var hedgeVolume = (double) (-positionPortfolio.BaseVolume);
+                var hedgeReferenceId = GenerateReferenceId(positionPortfolio);
+
+                _logger.LogInformation("Try to hedge position: {jsonText}",
+                    JsonConvert.SerializeObject(positionPortfolio));
+
+                new
+                {
+                    instrumentSettings.ExternalMarket, instrumentSettings.ExternalSymbol, hedgeSide, hedgeVolume,
+                    hedgeReferenceId
+                }.AddToActivityAsJsonTag("external-trade-request");
 
                 var trade = await market.MarketTrade(new MarketTradeRequest()
                 {
@@ -133,7 +143,7 @@ namespace Service.Liquidity.Engine.Domain.Services.Hedger
 
                 trade.AddToActivityAsJsonTag("external-trade-result");
 
-                _logger.LogInformation("Executed hedge trade. PositionId {positionId}. Trade: {tradeJson}", 
+                _logger.LogInformation("Executed hedge trade. PositionId {positionId}. Trade: {tradeJson}",
                     positionPortfolio.Id,
                     JsonConvert.SerializeObject(trade));
 
@@ -145,9 +155,9 @@ namespace Service.Liquidity.Engine.Domain.Services.Hedger
                 activity?.AddTag("hedge-result", "success");
 
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
-                _logger.LogError(ex, "Cannot hedge portfolio position. Position: {jsonText}. Settings: {settingsJson}", 
+                _logger.LogError(ex, "Cannot hedge portfolio position. Position: {jsonText}. Settings: {settingsJson}",
                     JsonConvert.SerializeObject(positionPortfolio),
                     JsonConvert.SerializeObject(instrumentSettings));
 
