676b5a0d71035b42ee54d2c895dd487aea589238
Alexey Novichikhin <novichikhin.au@gmail.com>
2021-05-17 16:37:31 +0300

    update servicebus sdk

--- src/Service.Liquidity.Engine.Client/AutofacHelper.cs
+++ src/Service.Liquidity.Engine.Client/AutofacHelper.cs
@@ -1,4 +1,10 @@
-﻿using Autofac;
+﻿using System.Collections.Generic;
+using Autofac;
+using DotNetCoreDecorators;
+using MyJetWallet.Sdk.ServiceBus;
+using MyServiceBus.Abstractions;
+using MyServiceBus.TcpClient;
+using Service.Liquidity.Engine.Domain.Models.Portfolio;
 using Service.Liquidity.Engine.Grpc;
 // ReSharper disable UnusedMember.Global
 
@@ -16,6 +22,36 @@ namespace Service.Liquidity.Engine.Client
             builder.RegisterInstance(factory.GetWalletPortfolioGrpc()).As<IWalletPortfolioGrpc>().SingleInstance();
             builder.RegisterInstance(factory.GetHedgeSettingsManagerGrpc()).As<IHedgeSettingsManagerGrpc>().SingleInstance();
             builder.RegisterInstance(factory.GetExternalMarketsGrpc()).As<IExternalMarketsGrpc>().SingleInstance();
+        }
+
+        public static ContainerBuilder RegisterPortfolioTradeSubscriber(this ContainerBuilder builder, MyServiceBusTcpClient client, string queryName, TopicQueueType queryType)
+        {
+            builder
+                .RegisterInstance(new MyServiceBusSubscriber<PortfolioTrade>(client, PortfolioTrade.TopicName, queryName, queryType, true))
+                .As<ISubscriber<IReadOnlyList<PortfolioTrade>>>()
+                .SingleInstance();
+
+            return builder;
+        }
+
+        public static ContainerBuilder RegisterPositionAssociationSubscriber(this ContainerBuilder builder, MyServiceBusTcpClient client, string queryName, TopicQueueType queryType)
+        {
+            builder
+                .RegisterInstance(new MyServiceBusSubscriber<PositionAssociation>(client, PositionAssociation.TopicName, queryName, queryType, true))
+                .As<ISubscriber<IReadOnlyList<PositionAssociation>>>()
+                .SingleInstance();
+
+            return builder;
+        }
+
+        public static ContainerBuilder RegisterPositionPortfolioSubscriber(this ContainerBuilder builder, MyServiceBusTcpClient client, string queryName, TopicQueueType queryType)
+        {
+            builder
+                .RegisterInstance(new MyServiceBusSubscriber<PositionPortfolio>(client, PositionPortfolio.TopicName, queryName, queryType, true))
+                .As<ISubscriber<IReadOnlyList<PositionPortfolio>>>()
+                .SingleInstance();
+
+            return builder;
         }
     }
 }
--- src/Service.Liquidity.Engine/Modules/ServiceBusModule.cs
+++ src/Service.Liquidity.Engine/Modules/ServiceBusModule.cs
@@ -1,45 +1,33 @@
 ﻿using Autofac;
 using DotNetCoreDecorators;
-using Microsoft.Extensions.Logging;
 using MyJetWallet.Sdk.Service;
+using MyJetWallet.Sdk.ServiceBus;
 using MyServiceBus.Abstractions;
-using MyServiceBus.TcpClient;
 using Service.Liquidity.Engine.Domain.Models.Portfolio;
-using Service.Liquidity.Engine.Domain.Services.ServiceBus;
 using Service.TradeHistory.Client;
 
 namespace Service.Liquidity.Engine.Modules
 {
     public class ServiceBusModule : Module
     {
-        public static ILogger ServiceBusLogger { get; set; }
-
         protected override void Load(ContainerBuilder builder)
         {
-            ServiceBusLogger = Program.LogFactory.CreateLogger(nameof(MyServiceBusTcpClient));
-
-            var serviceBusClient = new MyServiceBusTcpClient(Program.ReloadedSettings(e => e.SpotServiceBusHostPort), ApplicationEnvironment.HostName);
-            serviceBusClient.Log.AddLogException(ex => ServiceBusLogger.LogInformation(ex, "Exception in MyServiceBusTcpClient"));
-            serviceBusClient.Log.AddLogInfo(info => ServiceBusLogger.LogDebug($"MyServiceBusTcpClient[info]: {info}"));
-            serviceBusClient.SocketLogs.AddLogInfo((context, msg) => ServiceBusLogger.LogInformation($"MyServiceBusTcpClient[Socket {context?.Id}|{context?.ContextName}|{context?.Inited}][Info] {msg}"));
-            serviceBusClient.SocketLogs.AddLogException((context, exception) => ServiceBusLogger.LogInformation(exception, $"MyServiceBusTcpClient[Socket {context?.Id}|{context?.ContextName}|{context?.Inited}][Exception] {exception.Message}"));
-            
-            builder.RegisterInstance(serviceBusClient).AsSelf().SingleInstance();
-
+            var serviceBusClient = builder.RegisterMyServiceBusTcpClient(Program.ReloadedSettings(e => e.SpotServiceBusHostPort), ApplicationEnvironment.HostName, Program.LogFactory);
+                
             builder.RegisterTradeHistoryServiceBusClient(serviceBusClient, $"LiquidityEngine-{Program.Settings.ServiceBusQuerySuffix}", TopicQueueType.PermanentWithSingleConnection, true);
 
             builder
-                .RegisterInstance(new PortfolioTradePublisher(serviceBusClient))
+                .RegisterInstance(new MyServiceBusPublisher<PortfolioTrade>(serviceBusClient, PortfolioTrade.TopicName, false))
                 .As<IPublisher<PortfolioTrade>>()
                 .SingleInstance();
 
             builder
-                .RegisterInstance(new PositionAssociationPublisher(serviceBusClient))
+                .RegisterInstance(new MyServiceBusPublisher<PositionAssociation>(serviceBusClient, PositionAssociation.TopicName, false))
                 .As<IPublisher<PositionAssociation>>()
                 .SingleInstance();
 
             builder
-                .RegisterInstance(new PositionPortfolioPublisher(serviceBusClient))
+                .RegisterInstance(new MyServiceBusPublisher<PositionPortfolio>(serviceBusClient, PositionPortfolio.TopicName, false))
                 .As<IPublisher<PositionPortfolio>>()
                 .SingleInstance();
         }
